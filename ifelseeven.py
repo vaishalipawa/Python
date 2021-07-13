2b92f291464b27eac7a55877807569a58c4efb66
vaishali <vpgit9to5@gmail.com>
2021-07-13 12:59:33 +0100

    initial commit

---
+++ ifelseeven.py
@@ -0,0 +1,19 @@
+#Task
+#Given an integer,n , perform the following conditional actions:
+
+#If n  is odd, print Weird
+#If n is even and in the inclusive range of  2 to  5, print Not Weird
+#If n is even and in the inclusive range of 6 to 20, print Weird
+#If n is even and greater than 20 , print Not Weird
+
+n = int(input())
+if n %2 == 0:
+    print("not weird")
+elif n % 2 == 0 and 2<=n>=5:
+    print("not weird")
+elif n % 2 == 0 and 6>=n<=20:
+    print("weird")
+elif n % 2 == 0 and n<20:
+    print("Not weird")    
+else:
+    print("Enter a proper number")      
