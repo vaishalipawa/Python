060fa6d4e82d1f8155d420d4b8a88cbb62827255
vaishali <vpgit9to5@gmail.com>
2021-07-13 11:26:14 +0100

    initial commit

---
+++ 01_list.py
@@ -0,0 +1,12 @@
+a = [1,2,34,34,4,56,7,89]
+a[0] = 23
+print(a)
+print(a[3])
+a[5] = 60
+print(a)
+list = [45, "divya", 5.7 ]
+print(list)
+# list slicing
+friends = ["harry", "Tom", "Rohan", "sam", "45"]
+print(friends[-4:])
+
